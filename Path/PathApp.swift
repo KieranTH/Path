dd3ee94533c02d19148dc0e567bca86c0bfc410b
KieranTH <Kieran.hughes2@live.co.uk>
2021-07-11 14:36:42 +0100

    Initial Commit

---
+++ Path/PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathApp.swift
+//  Path
+//
+//  Created by Kieran on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
